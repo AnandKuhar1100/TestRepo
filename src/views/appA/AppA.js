261a1af6575217d667bdff9f506e982dcd7188a3
Anand Kumar <anand.kumar@housing.com>
2025-04-13 21:01:00 +0530

    New commit

--- src/views/appA/AppA.js
+++ src/views/appA/AppA.js
@@ -41,7 +41,6 @@ function AppA() {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text>Welcome to App A</Text>
-        <Text>This is a test project</Text>
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
